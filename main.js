405bba7fdb8f5e22ee0cd1e2473859db434c147e
Ingo Fischer <github@fischer-ka.de>
2020-05-01 00:23:21 +0200

    3.0.6

--- main.js
+++ main.js
@@ -133,30 +133,25 @@ function initWebServer(settings) {
                 res.end('Not Implemented');
             }, settings, adapter.config.certificates, adapter.config.leConfig, adapter.log);
 
-            try {
-                server.server.on('error', e => {
-                    if (e.toString().includes('EACCES') && port <= 1024) {
-                        adapter.log.error(`node.js process has no rights to start server on the port ${port}.\n` +
-                            `Do you know that on linux you need special permissions for ports under 1024?\n` +
-                            `You can call in shell following scrip to allow it for node.js: "iobroker fix"`
-                        );
-                    } else {
-                        adapter.log.error(`Cannot start server on ${settings.bind || '0.0.0.0'}:${port}: ${e}`);
-                    }
-                });
-
-                // Start the web server
-                server.server.listen(settings.port, (!settings.bind || settings.bind === '0.0.0.0') ? undefined : settings.bind || undefined);
-            } catch (e) {
+            let serverListening = false;
+            server.server.on('error', e => {
                 if (e.toString().includes('EACCES') && port <= 1024) {
-                    return adapter.log.error(`node.js process has no rights to start server on the port ${port}.\n` +
+                    adapter.log.error(`node.js process has no rights to start server on the port ${port}.\n` +
                         `Do you know that on linux you need special permissions for ports under 1024?\n` +
                         `You can call in shell following scrip to allow it for node.js: "iobroker fix"`
                     );
                 } else {
-                    return adapter.log.error(`Cannot start server on ${settings.bind || '0.0.0.0'}:${port}: ${e}`);
+                    adapter.log.error(`Cannot start server on ${settings.bind || '0.0.0.0'}:${port}: ${e}`);
                 }
-            }
+                if (!serverListening) {
+                    adapter.terminate ? adapter.terminate(1) : process.exit(1);
+                }
+            });
+
+            // Start the web server
+            server.server.listen(settings.port, (!settings.bind || settings.bind === '0.0.0.0') ? undefined : settings.bind || undefined, () => {
+                serverListening = true;
+            });
 
             settings.crossDomain     = true;
             settings.ttl             = settings.ttl || 3600;
